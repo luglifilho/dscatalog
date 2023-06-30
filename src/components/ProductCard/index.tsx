20c1bc3348da5f77f70d4c691f67139a27167aa1
luglifilho <luglifilho@hotmail.com>
2023-06-30 17:35:33 -0300

    props ProductCard

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,17 +2,23 @@ import './styles.css';
 
 import ProductImg from 'assets/images/product.png';
 import ProductPrice from 'components/ProductPrice';
+import { number } from 'yargs';
+import { Product } from '../../types/product';
 
-const ProductCard = () => {
+type Props = { 
+    product: Product;
+}
+
+const ProductCard = ({product}: Props) => {
 
     return (
         <div className="base-card product-card">
             <div className="card-top-container">
-                <img src={ProductImg} alt="Nome do produto" />
+                <img src={product.imgUrl} alt={product.name} />
             </div>
             <div className="card-bottom-container">
-                <h6>Nome do produto</h6>
-                <ProductPrice />
+                <h6>{product.name}</h6>
+                <ProductPrice price={product.price} />
             </div>
         </div>
     );
---
+++ src/types/product.ts
@@ -0,0 +1,13 @@
+import { Category } from "./category";
+
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgUrl:string;
+    date: string;
+    categories:  Category[];
+    
+}
+
