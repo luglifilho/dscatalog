0a58d1688ad2985738b4c8c6336ddf1b9bbd99eb
luglifilho <luglifilho@hotmail.com>
2023-07-03 04:20:29 -0300

    axios

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,20 @@
+import axios from 'axios';
 import './style.css';
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 
 const ProductDetaisl = () => {
+
+        let product: Product;
+        axios.get(BASE_URL + "/products/2")
+        .then(response => {
+            console.log(response.data)
+        });
+
     return (
         <div className='prodcutDetails-container'>
             <div className='base-card productDetails-card'>
