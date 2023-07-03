76adfeb33ae0853c5ee3bba67b30add841899b0f
luglifilho <luglifilho@hotmail.com>
2023-07-03 07:06:09 -0300

    loaders

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -2,18 +2,31 @@ import axios from 'axios';
 import './style.css';
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
+import { useEffect, useState } from 'react';
+
+
+type UrlParams = {
+    productId: string;
+}
 
 
 const ProductDetaisl = () => {
 
-        let product: Product;
-        axios.get(BASE_URL + "/products/2")
-        .then(response => {
-            console.log(response.data)
-        });
+    const { productId } = useParams<UrlParams>();
+
+    const [product, setProduct] = useState<Product>();
+
+        useEffect(() => {
+            axios.get(`${BASE_URL}/products/${productId}`)
+            .then(response => {
+                setProduct(response.data);
+            });
+        },[productId]);
+
+        
 
     return (
         <div className='prodcutDetails-container'>
@@ -28,19 +41,19 @@ const ProductDetaisl = () => {
                 <div className='row'>
                     <div className='col-xl-6'>
                         <div className='img-container'>
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" />
+                            <img src={product?.imgUrl} alt="Nome do Produto" />
 
                         </div>
                         <div className='name-price-container'>
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.67}/>
+                            <h1>{product?.name}</h1>
+                          { product && <ProductPrice price={product?.price}/>  }
                         </div>
 
                     </div>
                     <div className='col-xl-6'>
                         <div className='description-container'>
                             <h2>Descrição do Produto</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, doloremque.</p>
+                            <p>{product?.description}</p>
 
                         </div>
 
---
+++ frontend/src/types/vendor/spring.ts
@@ -0,0 +1,16 @@
+
+
+
+
+export type SpringPage<T> = {
+    content: T[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty: boolean;
+  };
+  
